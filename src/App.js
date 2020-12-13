ba288b9f5547a954db368fb0ed0deeeab224d1e6
Isracri <75814773+Isracri@users.noreply.github.com>
2020-12-12 23:53:57 -0800

    mi primer cambio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = 'World Azure class mx';
+  return <div>Hello Israel Corona {value}</div>;
 }
 
 export default App;
